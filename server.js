8ac706c0bb0b64ba0987b2a059b4640e4f74a830
edimhelena <edimhelenaguimaraes@gmail.com>
2025-07-11 07:58:05 -0300

    Criando html, rota 404, css e cardapio

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
+});
+
+// Middleware 404
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.listen(PORT, () => {
