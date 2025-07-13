470179a7f321bfaf47eb906513e93dc533f7a541
edimhelena <edimhelenaguimaraes@gmail.com>
2025-07-13 10:29:21 -0300

    criei html e css de contato, rotas de contato

--- server.js
+++ server.js
@@ -7,12 +7,28 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
+    res.status(200).sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get('/sugestao', (req, res) => {
   const {nome, ingredientes} = req.query
-  res.send(`<h1>Obrigada pela sugestão, ${nome}!</h1> <p>Ingredientes: ${ingredientes}</p> <p><a href="/">Voltar para o início</a></p>`)
+  res.status(200).send(`<h1>Obrigada pela sugestão, ${nome}!</h1> <p>Ingredientes: ${ingredientes}</p> <p><a href="/">Voltar para o início</a></p>`)
+})
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/contato.html', (req, res) =>{
+  res.status(200).sendFile(path.join(__dirname, "views", "contato.html"));
+});
+
+app.post('/contato', (req, res) => {
+  const {nome, email, assunto, mensagem} = req.body
+  res.status(200).send(`<h1>Mensagem Recebida</h1>
+    <p><strong>Nome:</strong> ${nome}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Assunto:</strong> ${assunto}</p>
+    <p><strong>Mensagem:</strong> ${mensagem}</p>
+    <p><a href="/">Voltar ao início</a></p>`)
 })
 
 // Middleware 404
